7a671f4bc04f817929640843f792904fab87f8c4
trunghq3101 <trunghq3101@gmail.com>
2022-09-19 11:27:48 +0700

    I want to draw 2 cards each turn until there is no card left

---
+++ test/card_deck_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+
+class Card {}
+
+class CardDeck {
+  CardDeck({this.fullLength = 110}) : currentLength = fullLength;
+
+  final int fullLength;
+  late int currentLength;
+
+  List<Card> draw() {
+    if (currentLength == 0) return [];
+    currentLength -= 2;
+    return [Card(), Card()];
+  }
+}
+
+void main() {
+  group('CardDeck test', () {
+    test('Deck must have 110 cards at first', () {
+      final deck = CardDeck();
+      expect(deck.fullLength, 110);
+    });
+
+    test('Draw 2 cards each turn until no card left', () {
+      final deck = CardDeck(fullLength: 4);
+      var cards = deck.draw();
+      expect(cards.length, 2);
+      expect(deck.currentLength, 2);
+      cards = deck.draw();
+      expect(cards.length, 2);
+      expect(deck.currentLength, 0);
+      cards = deck.draw();
+      expect(cards.length, 0);
+      expect(deck.currentLength, 0);
+    });
+  });
+}
