84bf5eac6566f131317f29005c3d93a196321f7b
trunghq3101 <trunghq3101@gmail.com>
2022-09-28 22:48:20 +0700

    add pause button

--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,3 +1,19 @@
 import 'package:flame/game.dart';
+import 'package:flame_svg/flame_svg.dart';
+import 'package:flutter/widgets.dart';
 
-class MainGame extends FlameGame {}
+class MainGame extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFFFFFFFF);
+
+  @override
+  Future<void>? onLoad() async {
+    final pauseIcon = await Svg.load('images/ic_pause.svg');
+    final pauseIconComponent = SvgComponent(
+      svg: pauseIcon,
+      position: Vector2.all(0),
+      size: Vector2.all(100),
+    );
+    await add(pauseIconComponent);
+  }
+}
