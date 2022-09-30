226dad2ea7105b157928d671e2aa83628405a4b7
trunghq3101 <trunghq3101@gmail.com>
2022-10-01 06:56:41 +0700

    load rive component

--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
-import 'package:flame_svg/flame_svg.dart';
+import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/widgets.dart';
+import 'package:rive/rive.dart';
 
 class MainGame extends FlameGame {
   @override
@@ -8,9 +9,13 @@ class MainGame extends FlameGame {
 
   @override
   Future<void>? onLoad() async {
-    final pauseIcon = await Svg.load('images/ic_pause.svg');
-    final pauseIconComponent = SvgComponent(
-      svg: pauseIcon,
+    final riveFile = RiveFile.asset('assets/images/btn_pause.riv');
+    final artboard = await loadArtboard(
+      riveFile,
+      artboardName: 'pause-one',
+    );
+    final pauseIconComponent = RiveComponent(
+      artboard: artboard,
       position: Vector2.all(0),
       size: Vector2.all(100),
     );
