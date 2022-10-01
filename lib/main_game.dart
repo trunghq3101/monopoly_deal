8fcf2c3dc107143702a2282b6b79a675015d7625
trunghq3101 <trunghq3101@gmail.com>
2022-10-01 07:14:38 +0700

    add tappable

--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,9 +1,18 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rive/rive.dart';
 
-class MainGame extends FlameGame {
+class PauseBtnComponent extends RiveComponent with Tappable {
+  PauseBtnComponent({required super.artboard})
+      : super(
+          position: Vector2.all(0),
+          size: Vector2.all(100),
+        );
+}
+
+class MainGame extends FlameGame with HasTappables {
   @override
   Color backgroundColor() => const Color(0xFFFFFFFF);
 
@@ -14,11 +23,7 @@ class MainGame extends FlameGame {
       riveFile,
       artboardName: 'pause-one',
     );
-    final pauseIconComponent = RiveComponent(
-      artboard: artboard,
-      position: Vector2.all(0),
-      size: Vector2.all(100),
-    );
+    final pauseIconComponent = PauseBtnComponent(artboard: artboard);
     await add(pauseIconComponent);
   }
 }
