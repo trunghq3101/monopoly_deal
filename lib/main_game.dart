a9a4d6af684dc04e1dd3aa3e44c194509eeb8401
trunghq3101 <trunghq3101@gmail.com>
2022-10-02 02:27:43 +0700

    chapter 3

--- lib/main_game.dart
+++ lib/main_game.dart
@@ -1,19 +1,45 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame_svg/flame_svg.dart';
+import 'package:flame/input.dart';
+import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/widgets.dart';
+import 'package:rive/rive.dart';
 
-class MainGame extends FlameGame {
+class PauseBtnComponent extends RiveComponent with Tappable {
+  PauseBtnComponent({required super.artboard})
+      : super(
+          position: Vector2.all(0),
+          size: Vector2.all(50),
+        );
+
+  late final RiveAnimationController _controller;
+
+  @override
+  Future<void>? onLoad() {
+    _controller = OneShotAnimation('Press', autoplay: false);
+    artboard.addController(_controller);
+    return super.onLoad();
+  }
+
+  @override
+  bool onTapDown(TapDownInfo info) {
+    _controller.isActive = true;
+    return true;
+  }
+}
+
+class MainGame extends FlameGame with HasTappables {
   @override
   Color backgroundColor() => const Color(0xFFFFFFFF);
 
   @override
   Future<void>? onLoad() async {
-    final pauseIcon = await Svg.load('images/ic_pause.svg');
-    final pauseIconComponent = SvgComponent(
-      svg: pauseIcon,
-      position: Vector2.all(0),
-      size: Vector2.all(100),
+    final riveFile = RiveFile.asset('assets/images/buttons.riv');
+    final artboard = await loadArtboard(
+      riveFile,
+      artboardName: 'pause-one',
     );
+    final pauseIconComponent = PauseBtnComponent(artboard: artboard);
     await add(pauseIconComponent);
   }
 }
