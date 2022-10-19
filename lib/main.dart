b2899e9db83911b57b07068c82a533e171ef8db9
trunghq3101 <trunghq3101@gmail.com>
2022-10-19 07:57:13 +0700

    add firebase and change bundle id

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:monopoly_deal/dev/repositories.dart';
 import 'package:monopoly_deal/pages/game_page.dart';
 import 'package:monopoly_deal/repositories/game_repository.dart';
 import 'package:monopoly_deal/routes.dart';
 
+import 'firebase_options.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -31,20 +33,34 @@ class MainApp extends StatelessWidget {
 }
 
 final _gameRepository = TestGameRepository();
+final _firebaseInit = Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 
 class DebugApp extends StatelessWidget {
   const DebugApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Row(
-        children: [
-          Expanded(child: MainApp(gameRepository: _gameRepository)),
-          const VerticalDivider(color: Colors.orange),
-          Expanded(child: MainApp(gameRepository: _gameRepository)),
-        ],
-      ),
-    );
+    return FutureBuilder<FirebaseApp>(
+        future: _firebaseInit,
+        builder: (_, snapshot) {
+          if (!snapshot.hasData) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            );
+          }
+          return MaterialApp(
+            home: Row(
+              children: [
+                Expanded(child: MainApp(gameRepository: _gameRepository)),
+                const VerticalDivider(color: Colors.orange),
+                Expanded(child: MainApp(gameRepository: _gameRepository)),
+              ],
+            ),
+          );
+        });
   }
 }
