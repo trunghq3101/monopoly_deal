369afece1bb30837043cc34d2ce696ff599e6202
Trung Hoang <trunghq3101@gmail.com>
2022-11-08 07:32:25 +0700

    refactor dashbook and add a story

--- lib/main_dashbook.dart
+++ lib/main_dashbook.dart
@@ -13,56 +13,65 @@ import 'dev/components.dart';
 void main() {
   final dashbook = Dashbook();
 
-  dashbook.storiesOf('2 players game').add(
-    'overview',
-    (ctx) {
-      final game = TableLayoutGame();
-      return GameWrapper(game: game);
-    },
-  ).add('tap outside', (ctx) {
-    final game = BaseGame()
-      ..onDebug((game) {
-        game.add(TappableOverlay());
-        game.add(TestComponent1()..size = Vector2.all(400));
-        game.add(TestComponent2()
-          ..size = Vector2.all(400)
-          ..position = Vector2.all(500));
-      });
-    return GameWrapper(game: game);
-  }).add(
-    'hand',
-    (ctx) {
-      final hand = Hand();
-      final game = BaseGame()
-        ..onDebug((game) {
-          game.add(TappableOverlay());
-          game.add(hand);
-        });
-      ctx
-        ..action('collapse', (_) {
-          hand.onCommand(Command(kTapOutsideHand));
-        })
-        ..action('expand', (_) {
-          hand.onCommand(Command(kTapInsideHand));
-        });
-      return GameWrapper(game: game);
-    },
-  ).add(
-    'holding cards',
-    (ctx) {
-      final hand = Hand();
-      final game = BaseGame()
-        ..onDebug((game) {
-          game.add(hand);
-        });
-      return GameWrapper(game: game);
-    },
-  ).add(
-    'opponent pick up cards',
-    (ctx) {
-      return Container();
-    },
-  );
+  dashbook
+      .storiesOf('2 players game')
+      .add('overview', _overview)
+      .add('tap outside', _tapOutside)
+      .add('hand', _hand)
+      .add('holding cards', _holdingCards)
+      .add('5 cards fly to hand', _fiveCardsFlyToHand);
 
   runApp(dashbook);
+}
+
+Widget _fiveCardsFlyToHand(ctx) {
+  final hand = Hand();
+  final game = BaseGame()
+    ..onDebug((game) {
+      game.add(hand);
+    });
+  return GameWrapper(game: game);
+}
+
+Widget _holdingCards(ctx) {
+  final hand = Hand();
+  final game = BaseGame()
+    ..onDebug((game) {
+      game.add(hand);
+    });
+  return GameWrapper(game: game);
+}
+
+Widget _hand(ctx) {
+  final hand = Hand();
+  final game = BaseGame()
+    ..onDebug((game) {
+      game.add(TappableOverlay());
+      game.add(hand);
+    });
+  ctx
+    ..action('collapse', (_) {
+      hand.onCommand(Command(kTapOutsideHand));
+    })
+    ..action('expand', (_) {
+      hand.onCommand(Command(kTapInsideHand));
+    });
+  return GameWrapper(game: game);
+}
+
+Widget _tapOutside(ctx) {
+  final game = BaseGame()
+    ..onDebug((game) {
+      game.add(TappableOverlay());
+      game.add(TestComponent1()..size = Vector2.all(400));
+      game.add(TestComponent2()
+        ..size = Vector2.all(400)
+        ..position = Vector2.all(500));
+    });
+  return GameWrapper(game: game);
+}
+
+Widget _overview(ctx) {
+  final game = TableLayoutGame();
+  return GameWrapper(game: game);
 }
