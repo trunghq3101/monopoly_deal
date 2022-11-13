459af784c7f2210510f42647e27eee7d6b105ce2
Trung Hoang <trunghq3101@gmail.com>
2022-11-13 10:19:43 +0700

    refactor targeting effect

--- lib/game_components/card_front.dart
+++ lib/game_components/card_front.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
-import 'package:flame/palette.dart';
 
 class CardFront extends SpriteComponent {
   CardFront({
@@ -23,19 +22,34 @@ class CardFront extends SpriteComponent {
 
   @override
   void render(Canvas canvas) {
+    final drawPosition = size / 2;
+    final drawSize = size + Vector2.all(15);
+    final delta = Anchor.center.toVector2()..multiply(drawSize);
+
+    final rRect = RRect.fromRectAndRadius(
+        (drawPosition - delta).toPositionedRect(drawSize),
+        const Radius.circular(24));
+
+    canvas.drawRRect(
+        rRect,
+        Paint()
+          ..color = const Color.fromARGB(146, 0, 0, 0)
+          ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 2));
+
     if (targeting) {
-      final drawPosition = size / 2;
-      final drawSize = size + Vector2.all(5);
-
-      final delta = Anchor.center.toVector2()..multiply(drawSize);
-      final drawRect = (drawPosition - delta).toPositionedRect(drawSize);
-
+      final targetingSize = drawSize + Vector2.all(15);
+      final targetingDelta = Anchor.center.toVector2()..multiply(targetingSize);
+      final targetingRect = RRect.fromRectAndRadius(
+          (drawPosition - targetingDelta).toPositionedRect(targetingSize),
+          const Radius.circular(28));
       canvas.drawRRect(
-          RRect.fromRectAndRadius(drawRect, const Radius.circular(20)),
-          BasicPalette.white.paint()
-            ..maskFilter = const MaskFilter.blur(BlurStyle.solid, 10));
+          targetingRect,
+          Paint()
+            ..color = const Color.fromARGB(255, 110, 240, 255)
+            ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 2));
     }
 
+    canvas.drawRRect(rRect, Paint()..color = const Color(0xFFFFFFFF));
     super.render(canvas);
   }
 }
