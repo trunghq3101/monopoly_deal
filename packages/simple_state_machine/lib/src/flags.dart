ca628a9517dfa6f37c52566b3fe8f75b9080c409
Trung Hoang <trunghq3101@gmail.com>
2022-11-25 14:16:36 +0700

    clean deps

--- packages/simple_state_machine/lib/src/flags.dart
+++
@@ -1,5 +0,0 @@
-part of simple_state_machine;
-
-class Flags {
-  static var debugMode = false;
-}
--- packages/simple_state_machine/lib/src/utils.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: avoid_print
-
-import '../simple_state_machine.dart';
-
-void printDebug(Object? s) {
-  if (!Flags.debugMode) return;
-  print(s);
-}
