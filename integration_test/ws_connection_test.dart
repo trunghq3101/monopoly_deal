496a352dae0ede2cd666f3f873fbd12ed6b4eb91
Trung Hoang <trunghq3101@gmail.com>
2022-12-23 09:02:59 +0700

    feat: creat room and join room

--- integration_test/ws_connection_test.dart
+++ integration_test/ws_connection_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:monopoly_deal/app/app.dart';
 import 'package:web_socket_client/web_socket_client.dart';
@@ -10,16 +12,34 @@ void main() {
       connection = WsConnection();
     });
 
-    tearDown(() {
-      connection.socket.close();
-    });
-
-    test('connect', () async {
+    test('connect', () {
       expect(
         connection.stateStream,
         emitsInOrder([const Connecting(), const Connected()]),
       );
       expect(connection.messageStream, emits(isA<ConnectedPacket>()));
+    });
+
+    test('create room', () {
+      connection.createRoom();
+
+      expect(connection.messageStream, emitsThrough(isA<CreatedRoomPacket>()));
+    });
+
+    test('join room', () async {
+      connection.createRoom();
+      final completer = Completer<int>();
+      connection.messageStream.listen((event) {
+        if (event is CreatedRoomPacket) {
+          completer.complete(event.roomId);
+        }
+      });
+      final roomId = await completer.future;
+
+      connection = WsConnection();
+      connection.joinRoom(roomId);
+
+      expect(connection.messageStream, emitsThrough(isA<JoinedRoomPacket>()));
     });
   });
 }
--- lib/app/logic/ws_connection.dart
+++ lib/app/logic/ws_connection.dart
@@ -1,11 +1,54 @@
+import 'dart:async';
+
 import 'package:monopoly_deal/app/app.dart';
 import 'package:web_socket_client/web_socket_client.dart';
 
 class WsConnection {
   final socket = WebSocket(Uri.parse("ws://localhost:3000/game"));
   final wsAdapter = WsAdapter();
+  String? sid;
 
-  Stream<ConnectionState> get stateStream => socket.connection;
-  Stream<ServerPacket> get messageStream =>
-      socket.messages.map((event) => wsAdapter.decode(event));
+  final _sidStreamController = StreamController<String>.broadcast();
+
+  Stream<ConnectionState> get stateStream =>
+      socket.connection.asBroadcastStream();
+  Stream<ServerPacket> get messageStream => socket.messages
+      .asBroadcastStream()
+      .map((event) => wsAdapter.decode(event));
+  Stream<String> get sidStream => _sidStreamController.stream;
+  final List<Function(String sid)> _pendingRequests = [];
+
+  WsConnection() {
+    sidStream.listen((event) {
+      sid = event;
+      for (var r in _pendingRequests) {
+        r(sid!);
+      }
+    });
+    messageStream.listen((event) {
+      if (event is ConnectedPacket) {
+        _sidStreamController.add(event.sid);
+      }
+    });
+  }
+
+  void createRoom() {
+    request(String sid) =>
+        socket.send(wsAdapter.encode(CreateRoomPacket(sid: sid)));
+    if (sid == null) {
+      _pendingRequests.add(request);
+      return;
+    }
+    request(sid!);
+  }
+
+  void joinRoom(int roomId) {
+    request(String sid) =>
+        socket.send(wsAdapter.encode(JoinRoomPacket(sid: sid, roomId: roomId)));
+    if (sid == null) {
+      _pendingRequests.add(request);
+      return;
+    }
+    request(sid!);
+  }
 }
