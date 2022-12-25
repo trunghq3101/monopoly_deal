422d83a0a64de4641a0ed1953f4dc4d70c7bc1a8
Trung Hoang <trunghq3101@gmail.com>
2022-12-25 22:11:29 +0700

    feat: layout for desktop

--- lib/app/waiting_room.dart
+++ lib/app/waiting_room.dart
@@ -7,73 +7,94 @@ class WaitingRoom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: Padding(
-          padding: const EdgeInsets.all(Insets.large),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton.icon(
-                  onPressed: () {
-                    Navigator.of(context).maybePop();
-                  },
-                  icon: const Icon(Icons.close),
-                  label: const Text("Leave"),
-                ),
-              ),
-              const SizedBox(height: Insets.medium),
-              Card(
-                elevation: 0,
-                color: Theme.of(context).colorScheme.tertiaryContainer,
-                child: Padding(
-                  padding: const EdgeInsets.all(Insets.medium),
-                  child: Text(
-                    'A8DIZ',
-                    style: Theme.of(context).textTheme.headline3?.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onTertiaryContainer),
-                    textAlign: TextAlign.center,
+      child: ColoredBox(
+        color: Theme.of(context).colorScheme.surface,
+        child: Align(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Scaffold(
+              backgroundColor:
+                  Theme.of(context).colorScheme.surfaceTint.withOpacity(0.05),
+              appBar: AppBar(
+                backgroundColor:
+                    Theme.of(context).colorScheme.surfaceTint.withOpacity(0.08),
+                leading: Align(
+                  child: TextButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).maybePop();
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("Leave"),
                   ),
                 ),
+                leadingWidth: 100,
               ),
-              Card(
-                elevation: 0,
-                color:
-                    Theme.of(context).colorScheme.surfaceTint.withOpacity(0.05),
-                child: Padding(
-                  padding: const EdgeInsets.all(Insets.medium),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          WaitingIndicator(),
-                          WaitingIndicator(),
-                        ],
+              body: Padding(
+                padding: const EdgeInsets.all(Insets.large),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: Insets.medium),
+                    Card(
+                      elevation: 0,
+                      color: Theme.of(context).colorScheme.tertiaryContainer,
+                      child: Padding(
+                        padding: const EdgeInsets.all(Insets.medium),
+                        child: Text(
+                          'A8DIZ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3
+                              ?.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onTertiaryContainer),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      const SizedBox(height: Insets.medium),
-                      const LinearProgressIndicator(),
-                    ],
-                  ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      color: Theme.of(context)
+                          .colorScheme
+                          .surfaceTint
+                          .withOpacity(0.08),
+                      child: Padding(
+                        padding: const EdgeInsets.all(Insets.medium),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                WaitingIndicator(),
+                                WaitingIndicator(),
+                              ],
+                            ),
+                            const SizedBox(height: Insets.medium),
+                            const LinearProgressIndicator(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor:
+                              Theme.of(context).colorScheme.onPrimary,
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                        ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+                        icon: const Icon(Icons.navigate_next_rounded),
+                        label: const Text('Start game'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const Spacer(),
-              Align(
-                alignment: Alignment.centerRight,
-                child: ElevatedButton.icon(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                  ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-                  icon: const Icon(Icons.navigate_next_rounded),
-                  label: const Text('Start game'),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
@@ -91,7 +112,7 @@ class WaitingIndicator extends StatelessWidget {
       child: Icon(
         Icons.person,
         size: 40,
-        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.08),
+        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.11),
       ),
     );
   }
