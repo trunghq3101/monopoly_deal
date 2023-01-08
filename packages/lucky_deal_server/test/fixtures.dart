09ca4a2763a894ca9c4a967e1a714d89540d66a4
Trung Hoang <trunghq3101@gmail.com>
2023-01-08 21:52:06 +0700

    chore: clean code

--- packages/lucky_deal_server/test/fixtures.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lucky_deal_server/providers/providers.dart';
-import 'package:redis/redis.dart';
-
-const testSid = 'a473ff7b-b3cd-4899-a04d-ea0fbd30a72e';
-final testConfig = Config(RedisConfig('localhost', 6379));
-Future<Command> testCommandGenerator() =>
-    RedisConnection().connect('localhost', 6379);
-const testRoomId = 'roomId';
